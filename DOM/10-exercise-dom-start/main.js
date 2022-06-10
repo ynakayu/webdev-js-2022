2a40416c1c639f85bef6fcad3b23c416673a2e1b
naka <9111754+ynakayu@users.noreply.github.com>
2022-06-10 15:05:08 +0900

    11-exercise-dom

--- DOM/10-exercise-dom-start/main.js
+++ DOM/10-exercise-dom-start/main.js
@@ -1,9 +1,8 @@
 'use strict';
 const e = document.getElementById('button');
 e.addEventListener('click', () => {
-  const textBox = document.getElementById('textBox');
   const li = document.createElement('li');
-  const text = document.createTextNode(textBox.value);
+  const text = document.createTextNode('test');
   li.appendChild(text);
   list.appendChild(li); // <li>test</li>
 }, false);
