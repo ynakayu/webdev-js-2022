72eee9ef555a5365a94ca32045142a60994d33aa
ynakayu <9111754+ynakayu@users.noreply.github.com>
2022-07-04 17:58:16 +0900

    27-array

---
+++ basics/27-array/main.js
@@ -0,0 +1,12 @@
+const colors = ['Red', 'Green', 'Blue'];
+// console.log(colors[0]);
+// console.log(colors[1]);
+// console.log(colors[2]);
+// console.log(colors[3]); // undefined
+
+// console.log(colors.length);
+for(let i = 0; i < colors.length; i++) {
+  console.log(colors[i]);
+}
+
+
